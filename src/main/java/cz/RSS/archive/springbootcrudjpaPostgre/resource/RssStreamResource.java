a8d84822190537b61844f1320395a6776a3e7aa3
Pavel Vodička <Vodouch.1@post.cz>
2024-02-24 16:00:05 +0100

    minor changes

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
@@ -20,9 +20,16 @@ public class RssStreamResource {
         Stream stream1 = new Stream(1,"teststream", "ccshmbhmb");
         Link link= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream1.getName()).withSelfRel();
         stream1.add(link);
-        Stream stream2 = new Stream(1,"teststream", "ccshmbhmb");
+        Stream stream2 = new Stream(2,"second", "cgee");
         Link link2= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream2.getName()).withSelfRel();
         stream2.add(link2);
         return Arrays.asList(stream1,stream2);
+    }
+    @GetMapping(value = "/1", produces = MediaTypes.HAL_JSON_VALUE)
+    public Stream getStream(){
+        Stream stream1 = new Stream(1,"teststream", "ccshmbhmb");
+        Link link= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream1.getName()).withSelfRel();
+        stream1.add(link);
+        return stream1;
     }
 }
