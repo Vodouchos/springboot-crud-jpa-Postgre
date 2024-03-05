1411cb828515df886b9ecb5f5ee4b05361922d9b
Pavel Vodička <Vodouch.1@post.cz>
2024-03-05 20:57:42 +0100

    mapping changes

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
@@ -3,12 +3,9 @@ package cz.RSS.archive.springbootcrudjpaPostgre.resource;
 import cz.RSS.archive.springbootcrudjpaPostgre.model.Stream;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.StreamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -19,23 +16,17 @@ public class RssStreamResource {
     @GetMapping(value = "/all", produces = MediaTypes.HAL_JSON_VALUE)
     public List<Stream> getAll(){
         return streamRepo.findAll();
-        /* Stream stream1 = new Stream(1,"teststream", "ccshmbhmb");
-        Link link= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream1.getName()).withSelfRel();
-        stream1.add(link);
-        Stream stream2 = new Stream(2,"second", "cgee");
-        Link link2= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream2.getName()).withSelfRel();
-        stream2.add(link2);
-        return Arrays.asList(stream1,stream2);*/
     }
-    @GetMapping(value = "/1", produces = MediaTypes.HAL_JSON_VALUE)
-    public Stream getStream(){
-        Stream stream1 = new Stream(1,"teststream", "ccshmbhmb");
-        Link link= WebMvcLinkBuilder.linkTo(RssStreamResource.class).slash(stream1.getName()).withSelfRel();
-        stream1.add(link);
-        return stream1;
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public Stream getStream(@PathVariable int id){
+        return streamRepo.getReferenceById(id);
     }
     @PostMapping(value = "/addstream")
     public void addStream(@RequestParam("name") String name, @RequestParam("url") String url){
         streamRepo.save(new Stream(name,url));
+    }
+    @PostMapping(value = "/removestream")
+    public void removeStream(@RequestParam("id") int id){
+        streamRepo.deleteById(id);
     }
 }
