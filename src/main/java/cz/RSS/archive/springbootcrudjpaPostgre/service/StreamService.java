872fffd4d748da596e921c35ac6651256abb7081
Pavel Vodička <Vodouch.1@post.cz>
2024-05-17 22:33:52 +0200

    Refactored StreamResource logic to StreamService

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/StreamService.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/StreamService.java
@@ -3,8 +3,6 @@ package cz.RSS.archive.springbootcrudjpaPostgre.service;
 import cz.RSS.archive.springbootcrudjpaPostgre.model.Stream;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.StreamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,14 +16,12 @@ public class StreamService {
     public Stream getStream(int id){
         return streamRepo.getReferenceById(id);
     }
-    public int addStream(String name,String url){
+    public void addStream(String name, String url){
         url = url.replaceFirst("^(https://www\\.|http://www\\.|http://|https://|www\\.)","");
         streamRepo.save(new Stream(name,url));
-        return 201;
     }
-    public int removeStream(int id){
+    public void removeStream(int id){
         streamRepo.deleteById(id);
-        return 200;
     }
 
 
