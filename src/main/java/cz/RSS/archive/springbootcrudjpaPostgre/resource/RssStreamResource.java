c339606bef69a9b379e53e1fdbbefc58f6ab9e17
Pavel Vodička <Vodouch.1@post.cz>
2024-06-22 11:50:14 +0200

    add logging

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/resource/RssStreamResource.java
@@ -3,6 +3,8 @@ package cz.RSS.archive.springbootcrudjpaPostgre.resource;
 import cz.RSS.archive.springbootcrudjpaPostgre.model.Stream;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.StreamRepository;
 import cz.RSS.archive.springbootcrudjpaPostgre.service.StreamService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +16,28 @@ import java.util.List;
 public class RssStreamResource {
     @Autowired
     private StreamRepository streamRepo;
+    Logger logger = LoggerFactory.getLogger(RssStreamResource.class);
     @Autowired
     private StreamService streamService;
     @GetMapping(value = "/all", produces = MediaTypes.HAL_JSON_VALUE)
     public List<Stream> getAll(){
+        logger.error("getAll called");
         return streamService.getAll();
     }
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     public Stream getStream(@PathVariable int id){
+        logger.trace("getStream called. ID: " + id);
         return streamService.getStream(id);
     }
     @PostMapping(value = "/addstream")
     public int addStream(@RequestParam("name") String name, @RequestParam("url") String url){
+        logger.info("addStream called. name: " + name + " url: " + url);
         streamService.addStream(name,url);
         return 201;
     }
     @DeleteMapping (value = "/removestream")
     public int removeStream(@RequestParam("id") int id){
+        logger.info("addStream called. id: " + id);
         streamService.removeStream(id);
         return 200;
     }
