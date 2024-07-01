be5281843c581739f053adc2c6cb0a3b97e2882e
Pavel Vodička <Vodouch.1@post.cz>
2024-07-02 00:12:22 +0200

    refactor

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/UpdateService.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/UpdateService.java
@@ -3,6 +3,7 @@ package cz.RSS.archive.springbootcrudjpaPostgre.service;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import cz.RSS.archive.springbootcrudjpaPostgre.model.RStream;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.ItemRepository;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.StreamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class UpdateService {
@@ -20,24 +22,23 @@ public class UpdateService {
     private ItemRepository itemRepo;
 
     public void updateRSSItemRepository(){
-        updateRSSItemRepository(2);//todo update all
+        streamRepo.findAll().forEach(this::updateRSSItemRepository);
     };
-
     public void updateRSSItemRepository(int id){
-        updateRSSItemRepository(id,streamRepo.getReferenceById(id).getUrl());
+        streamRepo.findById(id).ifPresent(this::updateRSSItemRepository);
     };
-    private void updateRSSItemRepository(int id, String url){
+    public void updateRSSItemRepository(RStream stream){
         try {
             SyndFeedInput input = new SyndFeedInput();
-            SyndFeed feed = input.build(new XmlReader(new URL(url)));
+            SyndFeed feed = input.build(new XmlReader(new URL(stream.getUrl())));
 
-            System.out.println(feed.getLink());
+            System.out.println(feed.getTitle());
 
         }
         catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("ERROR: "+ex.getMessage());
         }
+    }
 
-    };
 }
