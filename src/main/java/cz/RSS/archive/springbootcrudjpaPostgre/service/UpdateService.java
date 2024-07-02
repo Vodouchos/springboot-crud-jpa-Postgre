fde0a33b16e09d5e8b29fc69a7d84a335f2a2253
Pavel Vodička <Vodouch.1@post.cz>
2024-07-02 23:33:28 +0200

    new query for last update of RSS Stream items and demo usage

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/UpdateService.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/UpdateService.java
@@ -3,6 +3,7 @@ package cz.RSS.archive.springbootcrudjpaPostgre.service;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import cz.RSS.archive.springbootcrudjpaPostgre.model.RSSItem;
 import cz.RSS.archive.springbootcrudjpaPostgre.model.RStream;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.ItemRepository;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.StreamRepository;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -32,12 +34,19 @@ public class UpdateService {
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(new URL(stream.getUrl())));
 
-            System.out.println(feed.getTitle());
+            int streamId = stream.getId();
+            Date newestDBEntry = itemRepo.findFirstByStreamIdOrderByPubDateDesc(streamId)
+                    .map(RSSItem::getPubDate)
+                    .orElse(new Date(0L));
+
+            System.out.println("RSS " + stream.getName() + " loaded. Initializing update. StreamId: " +streamId + " newestEntry: " + newestDBEntry);
+            //todo logging
+            //todo write all new items in db
 
         }
         catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("ERROR: "+ex.getMessage());
+            ex.printStackTrace();//todo err handling and logging
+            System.out.println("ERRRRRRRRRRRRRRROR: "+ex.getMessage());
         }
     }
 
