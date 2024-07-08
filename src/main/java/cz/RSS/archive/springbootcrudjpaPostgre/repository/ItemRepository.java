0b5743a3d8470f10183c4708a3b5d660bad29726
Pavel Vodička <Vodouch.1@post.cz>
2024-07-08 18:14:43 +0200

    items can by retrieved by list of integers

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/repository/ItemRepository.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/repository/ItemRepository.java
@@ -4,9 +4,11 @@ import cz.RSS.archive.springbootcrudjpaPostgre.model.RSSItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource
 public interface ItemRepository extends JpaRepository<RSSItem, Integer> {
     Optional<RSSItem> findFirstByStreamIdOrderByPubDateDesc(int streamId);
+    List<RSSItem> findByStreamIdIn(List<Integer> streamId);
 }
