1d873a7cabb31e84eb80d56202ce3f1cf665d3dc
Pavel Vodička <Vodouch.1@post.cz>
2024-07-08 18:15:55 +0200

    split controller into controller and service - controller no longer directly access DB

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/controllers/RssItemsController.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/controllers/RssItemsController.java
@@ -2,6 +2,7 @@ package cz.RSS.archive.springbootcrudjpaPostgre.controllers;
 
 import cz.RSS.archive.springbootcrudjpaPostgre.model.RSSItem;
 import cz.RSS.archive.springbootcrudjpaPostgre.repository.ItemRepository;
+import cz.RSS.archive.springbootcrudjpaPostgre.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +13,16 @@ import java.util.List;
 @RequestMapping("/items")
 public class RssItemsController {
     @Autowired
-    private ItemRepository itemRepo;
-    @GetMapping(value = "/all", produces = MediaTypes.HAL_JSON_VALUE)
+    private ItemService itemService;
+    @GetMapping(value = "/", produces = MediaTypes.HAL_JSON_VALUE)
     public List<RSSItem> getAllItems(){
-        return itemRepo.findAll();
+        return itemService.getAll();
     }
-    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
-    public RSSItem getItem(@PathVariable int id){
-        return itemRepo.getReferenceById(id);
-    }
+
+    //todo get by id, optional update
+//    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+//    public RSSItem getItem(@PathVariable int id){
+//        return itemRepo.getReferenceById(id);
+//    }
 
 }
---
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/service/ItemService.java
@@ -0,0 +1,20 @@
+package cz.RSS.archive.springbootcrudjpaPostgre.service;
+
+import cz.RSS.archive.springbootcrudjpaPostgre.model.RSSItem;
+import cz.RSS.archive.springbootcrudjpaPostgre.model.RStream;
+import cz.RSS.archive.springbootcrudjpaPostgre.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepo;
+
+    public List<RSSItem> getAll(){
+        return itemRepo.findAll();
+    }
+}
