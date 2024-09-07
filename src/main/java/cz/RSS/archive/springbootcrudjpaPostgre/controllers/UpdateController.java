5691c5f755bcbd9fb0e7ba517858a0a954fe54e1
Pavel Vodička <Vodouch.1@post.cz>
2024-09-07 13:04:51 +0200

    added Scheduled job - update every 5min

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/controllers/UpdateController.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/controllers/UpdateController.java
@@ -4,6 +4,7 @@ import cz.RSS.archive.springbootcrudjpaPostgre.service.UpdateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ public class UpdateController {
     @Autowired
     private UpdateService updateService;
     Logger logger = LoggerFactory.getLogger(RssStreamController.class);
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void autoUpdate(){
+        logger.info("CRON Update");
+        updateService.updateRSSItemRepository();
+    }
     @GetMapping(value = "/update")
     public void updateAll(){
         logger.info("Called updateAll");
