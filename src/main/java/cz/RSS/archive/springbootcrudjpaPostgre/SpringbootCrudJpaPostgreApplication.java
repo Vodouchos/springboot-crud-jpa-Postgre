9000862025ffc7980d5427870f041c4ccf6c2965
Pavel Vodička <Vodouch.1@post.cz>
2024-09-07 10:09:08 +0200

    added EnableScheduling

--- src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/SpringbootCrudJpaPostgreApplication.java
+++ src/main/java/cz/RSS/archive/springbootcrudjpaPostgre/SpringbootCrudJpaPostgreApplication.java
@@ -2,8 +2,10 @@ package cz.RSS.archive.springbootcrudjpaPostgre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootCrudJpaPostgreApplication {
 
 	public static void main(String[] args) {
